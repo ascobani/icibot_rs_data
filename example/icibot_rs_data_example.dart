a6c3564e7fc0fdcc24fd91375e448290c6ad9a19
Furkan Ayyıldız <furkan199905@gmail.com>
2023-02-21 23:43:08 +0300

    received data from hive

--- example/icibot_rs_data_example.dart
+++ example/icibot_rs_data_example.dart
@@ -1,7 +1,25 @@
+import 'dart:io';
+
 import 'package:hive/hive.dart';
 import 'package:icibot_rs_data/src/manager/cache_manager/cache_manager.dart';
+import 'package:icibot_rs_data/src/manager/cache_manager/person.dart';
 import 'package:icibot_rs_data/src/service/rich_data_service.dart';
 
 void main() async {
-  await CacheManager().getData();
+  var path = Directory.current.path;
+  Hive
+    ..init(path)
+    ..registerAdapter(PersonAdapter());
+
+  var box = await Hive.openBox('testBox');
+
+  var person = Person(
+    name: 'Furkan',
+    age: 22,
+  );
+
+  await box.put('person', person);
+
+  Person personInfo = box.get("person");
+  print(personInfo.name);
 }
--- lib/src/manager/cache_manager/cache_manager.dart
+++ lib/src/manager/cache_manager/cache_manager.dart
@@ -1,12 +1,19 @@
+import 'dart:io';
+
 import 'package:hive/hive.dart';
 import 'package:icibot_rs_data/src/manager/cache_manager/person.dart';
 
 class CacheManager {
-  Future<void> getData() async {
-    // Hive.init(path)
+  late Box box;
+  var path = Directory.current.path;
+  Future<void> init() async {
+    Hive
+      ..init(path)
+      ..registerAdapter(PersonAdapter());
+    box = await Hive.openBox("box");
+  }
 
-    Box box = Hive.openBox("box") as Box;
-    Hive.registerAdapter(PersonAdapter());
+  Future<void> getData() async {
     await box.put("person", Person(name: "Furkan", age: 23));
     Person person = await box.get("person");
     print(person.name);
