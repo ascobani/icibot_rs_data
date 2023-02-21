6196b9b156866393237dba1ff7a1c43aba023282
Furkan Ayyıldız <furkan199905@gmail.com>
2023-02-21 16:33:34 +0300

    cache manager

--- lib/src/manager/cache_manager/cache_manager.dart
+++ lib/src/manager/cache_manager/cache_manager.dart
@@ -1,4 +1,11 @@
+import 'package:hive/hive.dart';
+
 class CacheManager {
-
-
+  Future<void> getData() async {
+    Hive.box("box");
+    Box box = Hive.openBox("box") as Box;
+    await box.put("isim", "furkan");
+    String name = await box.get("isim");
+    print(name);
+  }
 }
