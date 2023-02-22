c8524bb94c90943904621cce1dd23efc564d09c0
alisinancobani <cobanialisinan@gmail.com>
2023-02-22 11:40:55 +0300

    RSDataModel removed hive fields

--- example/icibot_rs_data_example.dart
+++ example/icibot_rs_data_example.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 
-import 'package:hive/hive.dart';
 import 'package:icibot_rs_data/src/manager/cache_manager/cache_manager.dart';
-import 'package:icibot_rs_data/src/manager/cache_manager/person.dart';
 import 'package:icibot_rs_data/src/service/rich_data_service.dart';
 
 void main() async {
